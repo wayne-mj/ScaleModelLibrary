3a3fc34963ff616c48d0ae6c51dc21e03fff006d
Wayne Jackson <warpkez@hotmail.com>
2024-01-17 07:58:22 +1000

    Merged tests into a single test

---
+++ Test-WMJ.ScaleModelLibrary/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using NUnit.Framework;
+global using WMJ.ScaleModelLibrary;
+global using WMJ.ScaleModelLibrary.MetricSystem;
+global using WMJ.ScaleModelLibrary.FractionSystem;
+global using WMJ.ScaleModelLibrary.ScaleMathematics;
